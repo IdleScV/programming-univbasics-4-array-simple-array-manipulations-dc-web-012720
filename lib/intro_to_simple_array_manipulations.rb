8f5da0cbe641e10532fb06438860678f31c5ad01
Wesley Chen <weschen1996@gmail.com>
2020-01-17 17:41:38 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+def using_push(array, string)
+  
+end
+
+def using_unshift(array, string)
+  
+end
+
+def using_pop(array)
+  
+end
+
+def pop_with_args(array)
+# remove last 2 arrays
+
+end
+
+def using_shift(array)
+  
+end
+
+def shift_with_args(array)
+# remove and return the first 2 items
+
+end
+
+def using_concat(arr1, arr2)
+
+end
+
+def using_insert(arr, element)
+
+end
+
+def using_uniq (array)
+  
+end
+
+def using_flatten(array)
+  
+end
+
+def using_delete(array, string)
+  
+end
+
+def using_delete_at(array, int)
+  
+end
